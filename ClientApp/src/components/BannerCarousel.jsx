7fefcb19c7e84f57a3c7df5e785ba63522d014cf
Ellvie <90103271+Ellvie@users.noreply.github.com>
2022-05-01 18:06:18 +0200

    banners

--- ClientApp/src/components/BannerCarousel.jsx
+++ ClientApp/src/components/BannerCarousel.jsx
@@ -2,9 +2,13 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 
-import banner1 from "../pics/testbanner1.jpg";
-import banner2 from "../pics/testbanner2.jpg";
-import banner3 from "../pics/testbanner3.jpg";
+import banner1 from "../pics/peopleBanner2.jpg";
+import banner2 from "../pics/mr.webbBanner.jpg";
+import banner3 from "../pics/bridgeTown.jpg";
+import banner4 from "../pics/motelCali.jpg";
+import banner5 from "../pics/miss.holmes.jpg";
+import banner6 from "../pics/sixStringDreams.jpg";
+import banner7 from "../pics/bangBangDang.jpg";
 
 export class BannerCarousel extends Component {
     static displayName = BannerCarousel.name;
@@ -12,7 +16,7 @@ export class BannerCarousel extends Component {
 
     render() {
         return (
-            <Carousel>
+            <Carousel showArrows={true} autoPlay={true} infiniteLoop={true} interval={7000} transitionTime={700}>
                 <div>
                     <img src={banner1} alt="Banner of TV-show." />
                 </div>
@@ -21,6 +25,18 @@ export class BannerCarousel extends Component {
                 </div>
                 <div>
                     <img src={banner3} alt="Banner of TV-show." />
+                </div>
+                <div>
+                    <img src={banner4} alt="Banner of TV-show." />
+                </div>
+                <div>
+                    <img src={banner5} alt="Banner of TV-show." />
+                </div>
+                <div>
+                    <img src={banner6} alt="Banner of TV-show." />
+                </div>
+                <div>
+                    <img src={banner7} alt="Banner of TV-show." />
                 </div>
             </Carousel>
         );
@@ -38,3 +54,7 @@ export class BannerCarousel extends Component {
 
 // Using html tag:
 // <link rel="stylesheet" href="<NODE_MODULES_FOLDER>/react-responsive-carousel/lib/styles/carousel.min.css"/>
+
+
+
+
